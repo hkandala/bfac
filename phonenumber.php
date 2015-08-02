3b0f173599d925baa5bad5773f52858b29e1f710
harish095 <kandalaharish95@gmail.com>
2015-08-03 03:15:20 +0530

    User Details page

--- phonenumber.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once 'include/php/connect.php';
-    $result=$GLOBALS['db']->raw('Select Name,Phoneno from users');
-    $i=1;
-    while($row = $result->fetch_assoc()) {
-        echo $i . ") Name: " . $row['Name'];
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Phone Number: ".$row['Phoneno'] . "<br>";
-        $i++;
-    }
