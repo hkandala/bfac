c24cc7ee21c983fea5335556ad5c52a6b890f60b
harish095 <kandalaharish95@gmail.com>
2015-08-02 22:53:21 +0530

    frontend done

--- detailsForm.php
+++ detailsForm.php
@@ -24,8 +24,50 @@
     require_once "header.php";
 ?>
 
-<div class="form-container">
-    
+<div class="form-container card-panel">
+    <h2>A few more details that we need...</h2>
+    <form class="details-form" action="detailsFormCheck.php" method="post">
+        <div class="col twelve">
+            <div class="input-field">
+                <i class="mdi-action-assessment prefix"></i>
+                <input type="text" name="no_of_hacks" id="no_of_hacks" class="validate"/>
+                <label for="no_of_hacks">Number of hacks you have previously attended</label>
+            </div>
+        </div>
+        <div class="col twelve">
+            <div class="input-field">
+                <i class="mdi-action-speaker-notes prefix"></i>
+                <textarea name="hack_desc" id="hack_desc" class="validate materialize-textarea"></textarea>
+                <label for = "hack_desc">Description and role you played for the hacks that you attended</label>
+            </div>
+        </div>
+        <div class="col twelve">
+            <div class="input-field">
+                <i class="mdi-communication-chat prefix"></i>
+                <textarea name="past_proj" id="past_proj" class="validate materialize-textarea"></textarea>
+                <label for = "past_proj">Past projects description and your role</label>
+            </div>
+        </div>
+        <div class="col twelve">
+            <div class="input-field">
+                <i class="mdi-action-announcement prefix"></i>
+                <textarea name="ideas" id="ideas" class="validate materialize-textarea"></textarea>
+                <label for = "ideas">Your idea for a socially assistive technology</label>
+            </div>
+        </div>
+        <div class="col twelve">
+            <div class="input-field">
+                <i class="mdi-communication-comment prefix"></i>
+                <input type="text" name="spec" id="spec" length="20" class="validate"/>
+                <label for = "spec">Specialization ( Web Dev / App Dev / Design etc. )</label>
+            </div>
+        </div>
+        <div class="col twelve">
+            <div class="input-field center-align">
+                <input type="submit" class="btn-large" value="Submit Details"/>
+            </div>
+        </div>
+    </form>
 </div>
 
 <?php
@@ -36,6 +78,95 @@
 <script src="include/js/jquery-1.11.3.min.js" type="text/javascript"></script>
 <script src="include/js/materialize.min.js" type="text/javascript"></script>
 <script src="include/js/app.js" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $(".details-form").submit(function (e) {
+            return detailsFormValidate();
+        });
+
+        $(".details-form #no_of_hacks").blur(function () {
+            if (!hacksValidate()) {
+                $(".details-form #no_of_hacks").addClass("invalid").removeClass("valid");
+            } else {
+                $(".details-form #no_of_hacks").addClass("valid").removeClass("invalid");
+            }
+        });
+        $(".details-form #hack_desc").blur(function () {
+            if (!hackDescValidate()) {
+                $(".details-form #hack_desc").addClass("invalid").removeClass("valid");
+            } else {
+                $(".details-form #hack_desc").addClass("valid").removeClass("invalid");
+            }
+        });
+        $(".details-form #past_proj").blur(function () {
+            if (!projValidate()) {
+                $(".details-form #past_proj").addClass("invalid").removeClass("valid");
+            } else {
+                $(".details-form #past_proj").addClass("valid").removeClass("invalid");
+            }
+        });
+        $(".details-form #ideas").blur(function () {
+            if (!ideasValidate()) {
+                $(".details-form #ideas").addClass("invalid").removeClass("valid");
+            } else {
+                $(".details-form #ideas").addClass("valid").removeClass("invalid");
+            }
+        });
+        $(".details-form #spec").blur(function () {
+            if (!specValidate()) {
+                $(".details-form #spec").addClass("invalid").removeClass("valid");
+            } else {
+                $(".details-form #spec").addClass("valid").removeClass("invalid");
+            }
+        });
+    });
+
+    function hacksValidate() {
+        var numbers = /[^0-9]/;
+        var cont = $(".details-form #no_of_hacks").val();
+        return (!numbers.test(cont) && cont.length != 0);
+    }
+
+    function hackDescValidate() {
+        var content = $(".details-form #hack_desc").val();
+        return (content.length > 5);
+    }
+
+    function projValidate() {
+        var content = $(".details-form #past_proj").val();
+        return (content.length > 5);
+    }
+
+    function ideasValidate() {
+        var content = $(".details-form #ideas").val();
+        return (content.length > 5);
+    }
+
+    function specValidate() {
+        var content = $(".details-form #spec").val();
+        return (content.length <= 20 && content.length != 0);
+    }
+
+    function detailsFormValidate () {
+        if(!hacksValidate()) {
+            $(".details-form #no_of_hacks").addClass("invalid").removeClass("valid");
+        }
+        if(!hackDescValidate()) {
+            $(".details-form #hack_desc").addClass("invalid").removeClass("valid");
+        }
+        if(!projValidate()) {
+            $(".details-form #past_proj").addClass("invalid").removeClass("valid");
+        }
+        if(!ideasValidate()) {
+            $(".details-form #ideas").addClass("invalid").removeClass("valid");
+        }
+        if(!specValidate()) {
+            $(".details-form #spec").addClass("invalid").removeClass("valid");
+        }
+
+        return (hacksValidate() && hackDescValidate() && projValidate() && ideasValidate() && specValidate());
+    }
+</script>
 <!------------------------------------------------------------------>
 </body>
 </html>
